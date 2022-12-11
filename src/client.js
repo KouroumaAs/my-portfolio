7ffa15749022c4c79c069f237397e94f248d1f07
KouroumaAs <boubasid2000@yahoo.fr>
2022-12-11 13:21:38 +0000

    adding in sanity studio

---
+++ src/client.js
@@ -0,0 +1,6 @@
+import sanityClient from '@sanity/client';
+
+export default sanityClient({
+    projectId: "of3lr5ec",
+    dataset: 'production'
+})
---
+++ studio/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'of3lr5ec',
+    dataset: 'production'
+  }
+})
